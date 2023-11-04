22f7c8665683b38ff9e9bd2638003befa53b1fd8
khajiitos <khajiit112@gmail.com>
2023-11-04 06:17:03 +0100

    1.9.1 for 1.20.2

--- Common/src/main/java/me/khajiitos/servercountryflags/common/mixin/serverbrowser/BrowsedEntryMixin.java
+++ Common/src/main/java/me/khajiitos/servercountryflags/common/mixin/serverbrowser/BrowsedEntryMixin.java
@@ -1,5 +1,7 @@
-package me.khajiitos.servercountryflags.common.mixin;
+package me.khajiitos.servercountryflags.common.mixin.serverbrowser;
 
+import com.epherical.serverbrowser.client.list.ServerBrowserList;
+import com.epherical.serverbrowser.client.screen.ServerBrowserScreen;
 import com.mojang.blaze3d.systems.RenderSystem;
 import me.khajiitos.servercountryflags.common.ServerCountryFlags;
 import me.khajiitos.servercountryflags.common.config.Config;
@@ -9,13 +11,12 @@ import me.khajiitos.servercountryflags.common.util.FlagRenderInfo;
 import me.khajiitos.servercountryflags.common.util.LocationInfo;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
-import net.minecraft.client.gui.screens.multiplayer.ServerSelectionList;
 import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -25,19 +26,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.ArrayList;
 import java.util.List;
 
-@Mixin(ServerSelectionList.OnlineServerEntry.class)
-public class OnlineServerEntryMixin {
-
-    @Shadow
-    @Final
+@Pseudo
+@Mixin(ServerBrowserList.BrowsedEntry.class)
+public class BrowsedEntryMixin {
+    @Shadow(remap = false)
     private ServerData serverData;
 
-    @Shadow
-    @Final
-    private JoinMultiplayerScreen screen;
+    @Shadow(remap = false) @Final private ServerBrowserScreen screen;
 
-    @ModifyArg(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Ljava/lang/String;IIIZ)I", ordinal = 0), method = "render", index = 2)
+    // Suppressing all warnings - don't know what the exact name is
+    // Minecraft Dev extension complains that it can't find the "render" function,
+    // because the project we are working with here is obfuscated
+    // When compiling on Forge/Fabric, the mod is deobfuscated there, so it works
+    @SuppressWarnings("all")
+    @ModifyArg(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Ljava/lang/String;III)I", ordinal = 0), method = "render", index = 2)
     public int serverNameX(int oldX) {
+        if (!Config.cfg.serverBrowserIntegration) {
+            return oldX;
+        }
+
         if (Config.cfg.flagPosition == FlagPosition.BEHIND_NAME) {
             APIResponse apiResponse = ServerCountryFlags.servers.get(serverData.ip);
             FlagRenderInfo renderInfo = ServerCountryFlags.getFlagRenderInfo(apiResponse);
@@ -50,8 +57,13 @@ public class OnlineServerEntryMixin {
         return oldX;
     }
 
+    @SuppressWarnings("all")
     @Inject(at = @At("TAIL"), method = "render")
     public void render(GuiGraphics context, int index, int y, int x, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean hovered, float tickDelta, CallbackInfo info) {
+        if (!Config.cfg.serverBrowserIntegration) {
+            return;
+        }
+
         APIResponse apiResponse = ServerCountryFlags.servers.get(serverData.ip);
         FlagRenderInfo flagRenderInfo = ServerCountryFlags.getFlagRenderInfo(apiResponse);
 
